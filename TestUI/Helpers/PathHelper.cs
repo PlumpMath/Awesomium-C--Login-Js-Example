1eed716b8bc6b43d39054bd2029c0702aa508c7a
prom3theu5 <dave.sekula@gmail.com>
2014-08-31 02:50:19 +0100

    Initial
    Example of how to handle js binding in c# and awesomium

---
+++ TestUI/Helpers/PathHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestUI.Helpers
+{
+    class PathHelper
+    {
+        public static string GetStartupPath()
+        {
+            String exePath = System.Reflection.Assembly.GetExecutingAssembly().GetModules()[0].FullyQualifiedName;
+            return System.IO.Path.GetDirectoryName(exePath);
+        }
+    }
+}
---
+++ TestUI/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Awesomium.Core;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using TestUI.Helpers;
+
+namespace TestUI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void BindMethods(IWebView _webView)
+        {
+            JSValue result = _webView.CreateGlobalJavascriptObject("app");
+            if (result.IsObject)
+            {
+                JSObject appObject = result;
+                appObject.Bind("login", false, new JavascriptMethodEventHandler(Login));
+            }
+        }
+
+        private void Login(object obj, JavascriptMethodEventArgs jsMethodArgs)
+        {
+            string username = jsMethodArgs.Arguments[0].ToString();
+            string password = jsMethodArgs.Arguments[1].ToString();
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("You must fill in both a username and a password");
+                return;
+            }
+            MessageBox.Show("Username: " + username + Environment.NewLine + "Password: " + password);
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            MainWebUI.ShowContextMenu += Html5_ShowContextMenu;
+            var source = new Uri(PathHelper.GetStartupPath() + "/Resources/login.html");
+            MainWebUI.Source = source;
+            BindMethods(MainWebUI);
+        }
+                     
+        void Html5_ShowContextMenu(object sender, Awesomium.Core.ContextMenuEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void StackPanel_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void ExitButton_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
